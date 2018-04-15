9fe191ae391801c2d0be8698a9d37db4b8aa86f0
Oleksii Lisikh <lizard5472@gmail.com>
2018-04-15 22:38:46 +0200

    Use directly rpm plugin

---
+++ rpm/script/preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Pre-install: started"
+echo "Package name = @pkgName@"
+echo "Package version = @pkgVersion@"
+echo "Pre-install: done"
--- script/preinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo Pre-install run!
-echo Package name = @pkgName@
-echo Package version = @pkgVersion@
